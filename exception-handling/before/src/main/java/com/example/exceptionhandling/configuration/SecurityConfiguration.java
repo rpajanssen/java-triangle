d162d560fdec140747315ecb0b1ed6fc072facfe
Roger Janssen <roger.janssen@nl.abnamro.com>
2020-06-25 16:55:16 +0200

    added a some examples and documentation

--- exception-handling/before/src/main/java/com/example/exceptionhandling/configuration/SecurityConfiguration.java
+++ exception-handling/before/src/main/java/com/example/exceptionhandling/configuration/SecurityConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-// NOTE : example 13 - Handle access denied exceptions
+// NOTE : example 13 - Handle access denied to resources exceptions
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
